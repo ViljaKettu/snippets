e47fe34f242a06b8a4de1d19a4b7fd4e9876c28d
ViljaKettu <31925883+ViljaKettu@users.noreply.github.com>
2019-03-22 17:01:57 +0200

    Wizard game snippets

---
+++ Magick The Game/Assets/_Scripts/ViljanScriptit/Floors.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct Floors 
+{
+    string name;
+    int floorId;
+    float sizeX, sizeY;
+
+    public Floors(string _name, int _floorId, float _sizeX, float _sizeY)
+    {
+        name = _name;
+        floorId = _floorId;
+        sizeX = _sizeX;
+        sizeY = _sizeY;
+    }
+}
